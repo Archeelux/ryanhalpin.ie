1fa847907bc53cf07efa479d7a419c849500da93
Arturs <archeelux@gmail.com>
2019-10-18 20:50:26 +0100

    re added old design with new touches

--- .eleventy.js
+++ .eleventy.js
@@ -1,10 +1,16 @@
+const util = require("util");
+
 module.exports = function(eleventyConfig) {
-  eleventyConfig.addPassthroughCopy('src/images')
+  eleventyConfig.addPassthroughCopy("src/images");
+
+  eleventyConfig.addFilter("dump", obj => {
+    return util.inspect(obj);
+  });
 
   return {
-    dir: { input: 'src', output: 'dist', data: '_data' },
+    dir: { input: "src", output: "dist", data: "_data" },
     passthroughFileCopy: true,
-    templateFormats: ['njk', 'md', 'css', 'html', 'yml'],
-    htmlTemplateEngine: 'njk'
-  }
-}
+    templateFormats: ["njk", "md", "css", "html", "yml"],
+    htmlTemplateEngine: "njk"
+  };
+};
