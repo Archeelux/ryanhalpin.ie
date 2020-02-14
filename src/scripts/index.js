dc58cd475431d8540297d71403d1ff0459d37115
Archeelux <archeelux@gmail.com>
2020-02-14 14:51:50 +0000

    added gallery

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -6,24 +6,71 @@ var lazyLoadInstance = new LazyLoad({
 });
 
 document.addEventListener("DOMContentLoaded", function(event) {
-  const images = Array.from(document.querySelectorAll(".magnify-outer"));
-  if (!images.length) return;
+  // Modals
 
-  const modal = document.querySelector(".modal");
-  const modalImage = document.querySelector(".modal .modal-content p img");
-  const modalClose = document.querySelector(".modal-close");
+  var rootEl = document.documentElement;
+  var allModals = getAll(".modal");
+  var modalButtons = getAll(".modal-button");
+  var modalCloses = getAll(
+    ".modal-background, .modal-close, .modal-card-head .delete, .modal-card-foot .button"
+  );
 
-  const onClickImage = e => {
-    const imgLink = e.target.dataset.imglink;
+  if (modalButtons.length > 0) {
+    modalButtons.forEach(function(el) {
+      el.addEventListener("click", function() {
+        var target = document.getElementById(el.dataset.target);
+        rootEl.classList.add("is-clipped");
+        target.classList.add("is-active");
+      });
+    });
+  }
 
-    modalImage.dataset.src = imgLink + "?nf_resize=fit&w=&h=100";
-    modalImage.src = imgLink;
+  if (modalCloses.length > 0) {
+    modalCloses.forEach(function(el) {
+      el.addEventListener("click", function() {
+        closeModals();
+      });
+    });
+  }
 
-    console.log(modalImage);
+  document.addEventListener("keydown", function(event) {
+    var e = event || window.event;
+    if (e.keyCode === 27) {
+      closeModals();
+    }
+  });
 
-    lazyLoadInstance.update();
-    modal.classList += "is-active";
-  };
+  function closeModals() {
+    rootEl.classList.remove("is-clipped");
+    allModals.forEach(function(el) {
+      el.classList.remove("is-active");
+    });
+  }
 
-  images.forEach(a => (a.onclick = onClickImage));
+  // Functions
+
+  function getAll(selector) {
+    return Array.prototype.slice.call(document.querySelectorAll(selector), 0);
+  }
+
+  // const images = Array.from(document.querySelectorAll(".magnify-outer"));
+  // if (!images.length) return;
+
+  // const modal = document.querySelector(".modal");
+  // const modalImage = document.querySelector(".modal .modal-content p img");
+  // const modalClose = document.querySelector(".modal-close");
+
+  // const onClickImage = e => {
+  //   const imgLink = e.target.dataset.imglink;
+
+  //   modalImage.dataset.src = imgLink + "?nf_resize=fit&w=&h=100";
+  //   modalImage.src = imgLink;
+
+  //   console.log(modalImage);
+
+  //   lazyLoadInstance.update();
+  //   modal.classList += "is-active";
+  // };
+
+  // images.forEach(a => (a.onclick = onClickImage));
 });
