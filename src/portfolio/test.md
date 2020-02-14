8c784ce44ebbc3f8d717deb165ea5083199c28c3
Archeelux <archeelux@gmail.com>
2020-02-14 14:42:35 +0000

    first

--- src/portfolio/test.md
+++ src/portfolio/test.md
@@ -1,18 +1,11 @@
 ---
 date: 2020-02-14T13:26:30Z
 tags:
-- portfolio
+  - portfolio
 layout: layouts/work.njk
 title: Test
 featured: true
 image: "/images/rennyaccient.jpg"
 gallery:
-- "/images/photo4.jpg"
-- "/images/photo18.jpg"
-- "/images/photo16.jpg"
-- "/images/24greek.jpg"
-- "/images/photo20.jpg"
-- "/images/photo31.jpg"
-
----
-LMAO THIS IS TEST M80 get KEKT
+  - "/images/photo4.jpg"
+---LMAO THIS IS TEST M80 get KEKT
--- src/scripts/index.js
+++ src/scripts/index.js
@@ -4,3 +4,26 @@ var lazyLoadInstance = new LazyLoad({
   elements_selector: ".lazy"
   // ... more custom settings?
 });
+
+document.addEventListener("DOMContentLoaded", function(event) {
+  const images = Array.from(document.querySelectorAll(".magnify-outer"));
+  if (!images.length) return;
+
+  const modal = document.querySelector(".modal");
+  const modalImage = document.querySelector(".modal .modal-content p img");
+  const modalClose = document.querySelector(".modal-close");
+
+  const onClickImage = e => {
+    const imgLink = e.target.dataset.imglink;
+
+    modalImage.dataset.src = imgLink + "?nf_resize=fit&w=&h=100";
+    modalImage.src = imgLink;
+
+    console.log(modalImage);
+
+    lazyLoadInstance.update();
+    modal.classList += "is-active";
+  };
+
+  images.forEach(a => (a.onclick = onClickImage));
+});
